4600b0f4cb68599762256d305b9b01610c1b5ad0
Ana Islas <apislasm@gmail.com>
2025-07-03 00:52:36 -0600

    array op OK, hacer interpretador de logger

--- test_array_operations.py
+++ test_array_operations.py
@@ -1,31 +1,43 @@
 def oneTimeAlgorithm():
     pass
 
-def buildFileLogger():
-    pass
+def buildFileLogger(surfaceName, runName):
+    # nombre de columnas
+    header= ["t, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18\n"] # construir con motors[i].ID
+
+    fn = "LOGS/" + surfaceName + "/" + runName + "_POSITION.csv";     posFile = open(fn, "w")
+    fn = "LOGS/" + surfaceName + "/" + runName + "_SPEED.csv";        speFile = open(fn, "w")
+    fn = "LOGS/" + surfaceName + "/" + runName + "_CURRENT.csv";      curFile = open(fn, "w")
+    fn = "LOGS/" + surfaceName + "/" + runName + "_VOLTAGE.csv";      volFile = open(fn, "w")
+    fn = "LOGS/" + surfaceName + "/" + runName + "_TEMPERATURE.csv";  temFile = open(fn, "w")
+
+    posFile.writelines(header)
+    speFile.writelines(header)
+    curFile.writelines(header)
+    volFile.writelines(header)
+    temFile.writelines(header)
+
+    return posFile, speFile, curFile, volFile, temFile
+
+def closeFileLogger(files):
+    for i in range(0,len(files)):
+        files[i].close()
+
+def writeToFIle(filename, data):
+    filename.writelines(data)
 
 def readFileLogger():
     pass
 
 ## -------------------------------------------------------------------------------
 
-# nombre de columnas
-header= ["t, 12, 13, 14, \n", "t, 12, 13, 14, \n"] # construir con motors[i].ID
-
 # Name of the CSV file
-surfaceName = "GRASS"
+surfaceName = "FLAT"
 runName = "TEST1"
 
-fn = "LOGS/" + surfaceName + "/" + runName + "_POSITION.csv";     posFile = open(fn, "w")
-fn = "LOGS/" + surfaceName + "/" + runName + "_SPEED.csv";        speFile = open(fn, "w")
-fn = "LOGS/" + surfaceName + "/" + runName + "_CURRENT.csv";      curFile = open(fn, "w")
-fn = "LOGS/" + surfaceName + "/" + runName + "_VOLTAGE.csv";      volFile = open(fn, "w")
-fn = "LOGS/" + surfaceName + "/" + runName + "_TEMPERATURE.csv";  temFile = open(fn, "w")
+#posFile, speFile, curFile, volFile, temFile = buildFileLogger(surfaceName, runName)
+#files = [posFile, speFile, curFile, volFile, temFile]
 
-posFile.writelines(header)
+#writeToFIle(posFile, "t, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18\n")
 
-posFile.close()
-speFile.close()
-curFile.close()
-volFile.close()
-temFile.close()
+#closeFileLogger(files)
